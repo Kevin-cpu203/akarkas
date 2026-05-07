8ce537b365bad6e42a9a251e1025673ffd05536b
Revalino <ardanakevin48@gmail.com>
2026-05-07 12:29:53 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'akarkas-pro-v1';
+const cacheName = 'akarkas-pro-v2'; // Naikkan versinya jadi v2
 const assets = [
   './',
   './index.html',
@@ -9,15 +9,29 @@ const assets = [
   './assets/icon_perusahaan.png'
 ];
 
+// 1. Install: Menabung aset
 self.addEventListener('install', e => {
+  self.skipWaiting(); // Paksa SW baru langsung aktif
   e.waitUntil(
     caches.open(cacheName).then(cache => {
-      console.log('AkarKas: Menabung aset ke memori HP...');
       return cache.addAll(assets);
     })
   );
 });
 
+// 2. Activate: Hapus sampah cache lama
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+      );
+    })
+  );
+  return self.clients.claim(); // Langsung ambil kendali halaman
+});
+
+// 3. Fetch: Ambil dari tabungan (Offline)
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(res => {
